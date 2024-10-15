2dae7c2e3ec716348cc7e0b169361d5d59b5f6f7
Rodnesia Goodloe <rodnesiagoodloe@rodnesias-mbp.main.local.una.edu>
2024-10-15 15:36:23 -0500

    dev mary

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//const's
+// const's
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,12 +9,12 @@ const PORT = process.env.PORT || 10000;
 // MongoDB connection
 const uri = `mongodb+srv://nesiagoodloe03:${process.env.MONGO_PWD}@cluster0.pff8r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-//app.uses
+// app.uses
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('./public/'));
 
-//mongo client
+// mongo client
 const client = new MongoClient(uri);
 
 // connect to MongoDB
@@ -63,15 +63,64 @@ app.post('/submit', async (req, res) => {
 app.get('/read', async (req, res) => {
   try {
     const results = await client.db("nesias-db").collection("my-collection").find({}).toArray();
-    console.log('Fetched results:', results); // log the results to check their structure
-    res.render('results', { data: results });
+    res.render('results', { data: results }); // Pass the data to the EJS view
   } catch (err) {
     console.error('Error fetching data:', err);
     res.status(500).send('Error retrieving data');
   }
 });
 
+// update form
+app.get('/update/:id', async (req, res) => {
+  const { id } = req.params;
 
+  try {
+    const response = await client.db("nesias-db").collection("my-collection").findOne({ _id: new ObjectId(id) });
+    res.render('update', { response }); // Pass the fetched response to the update view
+  } catch (err) {
+    console.error('Error fetching data for update:', err);
+    res.status(500).send('Error retrieving data for update');
+  }
+});
+
+// handle update 
+app.post('/update/:id', async (req, res) => {
+  const { id } = req.params;
+  const { Fname, Lname, date, phonenum, Lnumber, activities, landmark, dest, places } = req.body;
+
+  const updatedData = {
+    firstName: Fname,
+    lastName: Lname,
+    date: date,
+    phoneNumber: phonenum,
+    lNumber: Lnumber,
+    activities: Array.isArray(activities) ? activities : [], // This will now work correctly
+    landmark: landmark || '',
+    foodChoice: dest || '',
+    places: places || ''
+  };
+
+  try {
+    await client.db("nesias-db").collection("my-collection").updateOne({ _id: new ObjectId(id) }, { $set: updatedData });
+    res.redirect('/read');
+  } catch (err) {
+    console.error('Error updating data:', err);
+    res.status(500).send('Error updating data');
+  }
+});
+
+// handle delete 
+app.post('/delete/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await client.db("nesias-db").collection("my-collection").deleteOne({ _id: new ObjectId(id) });
+    res.redirect('/read');
+  } catch (err) {
+    console.error('Error deleting data:', err);
+    res.status(500).send('Error deleting data');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running & listening on port ${PORT}`);
